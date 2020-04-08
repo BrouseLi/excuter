6af1a023d9982e3a7f810961686cf83d86477b30
brouseli <386131379@qq.com>
2020-04-08 17:45:20 +0800

    add VoterController updateSoftWare

---
+++ src/main/java/com/policy/excuter/ExcuterApplication.java
@@ -0,0 +1,13 @@
+package com.policy.excuter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcuterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExcuterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/policy/excuter/ExcuterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.policy.excuter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcuterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
